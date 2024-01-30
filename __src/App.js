e2671e05fb2f4517b29fc148338d74c2508d298f
Ivan Reynoso <ireynoso760@gmail.com>
2024-01-30 10:22:04 -0800

    CTC-34 First commit

---
+++ __src/App.js
@@ -0,0 +1,15 @@
+// src/components/paw patrol
+
+import React from 'react';
+
+const PawPatrol = () => {
+  return (
+    <div>
+      <h1>Dog Walking Service</h1>
+      <p>Welcome to our dog walking service! We love taking care of your furry friends.</p>
+      {/* Add more components and features as needed */}
+    </div>
+  );
+}
+
+export default PawPatrol;
---
+++ __src/PawPatrol.js
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from 'react';
+import PawPatrol from './components/PawPatrol';
+
+function App() {
+  return (
+    <div className="App">
+      <PawPatrol />
+    </div>
+  );
+}
+
+export default App;
